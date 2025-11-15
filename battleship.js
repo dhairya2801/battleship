9a92bdc3223983329e3d5c38fad5c1c0c2bc22da
Dhairya <dhairyashah.ai@gmail.com>
2025-11-15 01:05:38 -0500

    Basic drag and drop added

--- battleship.js
+++ battleship.js
@@ -19,3 +19,32 @@ boards.addEventListener('click', (e) => {
         console.log(e.target.dataset.row, e.target.dataset.col);
     }
 })
+
+const ships = document.querySelectorAll('.ship');
+ships.forEach(ship => {
+    ship.addEventListener('dragstart', (e) => {
+        e.dataTransfer.setData('text/plain', e.target.id);
+    })
+});
+
+board1.addEventListener('dragover', (e) => {
+    e.preventDefault();
+})
+board1.addEventListener('drop', (e) => {
+    e.preventDefault();
+    // getBoundingClientRect gives the position of board1 relative to the viewport
+    const x = e.clientX - board1.getBoundingClientRect().left;
+    const y = e.clientY - board1.getBoundingClientRect().top;
+    
+    const snapLeft = Math.floor(x/40) * 40;
+    const snapTop = Math.floor(y/40) * 40;
+    
+    const id = e.dataTransfer.getData('text/plain');
+    const draggableElement = document.getElementById(id);
+    board1.appendChild(draggableElement);
+    draggableElement.style.cursor = 'default';
+
+    draggableElement.style.position = 'absolute';
+    draggableElement.style.left = `${snapLeft}px`;
+    draggableElement.style.top = `${snapTop}px`;
+})
