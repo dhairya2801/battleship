173a02c8309df020eeb2ee0d52882b0b04e5baef
Dhairya <dhairyashah.ai@gmail.com>
2025-11-16 03:24:50 -0500

    drag and drop fn complete

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 // TODO:
 // 1. when a ship is placed; 
-// - store the cell coordinates to indicate during hit
 // - disable the surrounding cells
-// - allow 90 deg rotate on click
-// 2. handle ship overflowing outside the board
+// 2. handle ship edge cases
+// - overflowing outside the board
+// - overlapping on other ships
+// - being placed on adjacent cells
 // 3. add reset and randomize buttons
 // 4. make a test run
 
 const boards = document.getElementById('boards');
 const board1 = document.getElementById('board1');
 const board2 = document.getElementById('board2');
+const frame2 = document.getElementById('frame2');
+const shipDock = document.getElementById('ship-dock');
+
 createBoard(board1);
 createBoard(board2);
+randomizeComputer();
 
 function createBoard(board) {
     for (let i=0; i<100; i++) {
@@ -34,9 +39,6 @@ board1.addEventListener('dragover', (e) => {
     e.preventDefault();
 })
 
-board1.addEventListener('dragleave', (e) => {
-
-})
 board1.addEventListener('drop', (e) => {
     e.preventDefault();
     // getBoundingClientRect gives the position of board1 relative to the viewport
@@ -59,9 +61,57 @@ board1.addEventListener('drop', (e) => {
     // add placed class
     draggableElement.classList.add('placed');
     // store coordinates
-    setShip(snapLeft, snapTop, id);
+    setShip(snapLeft/40, snapTop/40, draggableElement);
 })
 
-function setShip(left, top, id) {
+board1.addEventListener('click', (e) => {
+    if (e.target.classList.contains('placed')) {
+        const shipEle = e.target;
+        shipEle.classList.toggle('rotated');
+        setShip(parseInt(shipEle.style.left.slice(0, -2), 10)/40, 
+            parseInt(shipEle.style.top.slice(0, -2), 10)/40, shipEle);
+    }
+})
+
+const shipCoords1 = new Map();
+const shipCoords2 = new Map();
+
+function setShip(left, top, ele) {
+    const id = ele.id;
+    const isRotated = ele.classList.contains('rotated');
+    let length = 0;
+    shipCoords1.set(id, []);
     
+    if (id === "sa") {length = 4;}
+    else if (id === "sb" || id === "sc") {length = 3;}
+    else if (id === "sd" || id === "se" || id === "sf") {length = 2;}
+    else {length = 1;}
+    
+    for (let i=0; i<length; i++) {
+        if (isRotated) {
+            shipCoords1.get(id).push([top+i, left]);
+        }
+        else {
+            shipCoords1.get(id).push([top, left+i]);
+        }
+    }
+}
+
+const playBtn = document.getElementById('play');
+const info = document.querySelector('.info');
+playBtn.addEventListener('click', () => {
+    frame2.classList.toggle('hidden');
+    shipDock.classList.toggle('hidden');
+    info.textContent = 'Your turn. Fire on the computer\'s board.';
+    if (shipCoords1.size === 10) {
+        startGame();
+    }
+})
+
+function startGame() {
+    
+}
+
+function randomizeComputer() {
+
 }
